2b23881430b0ca23aa4d8be63eaa19e720b639ea
Tetiana-Banshchykova <tetiana.banshchykova.ap.2023@lpnu.ua>
2026-02-17 13:54:33 +0200

    Create command_list.c
    Created command placeholders

---
+++ STM/mahjong-game/Core/Src/command_list.c
@@ -0,0 +1,38 @@
+/*
+ * command_list.c
+ *
+ *  Created on: 17 лют. 2026 р.
+ *      Author: home
+ */
+/*
+ *  #define CMD_START 0x01
+	#define CMD_RESET 0x02
+	#define CMD_SHUFFLE 0x03
+	#define CMD_SELECT 0x04
+	#define CMD_MATCH 0x05
+ */
+void cmd_reset()
+{
+
+}
+
+void cmd_shuffle()
+{
+
+}
+
+void cmd_select()
+{
+
+}
+
+void cmd_match()
+{
+
+}
+
+
+int main()
+{
+
+}
